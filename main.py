36375e88383f46c1363e34f78936cd71240c42b6
LucasPego3 <lukpego@icloud.com>
2024-09-16 18:11:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ def formativa1(nome):
     api = "https://pokeapi.co/api/v2/"
     url = api + "pokemon/" + nome
     res = requests.get(url)
-    print(res)
+
     if res.status_code == 200:
         pokemon = res.json()
         return pokemon
     else:
         print(f"Pokemon não encontrado! Erro: {res.status_code}")
 
-pokemon = "pikachu"
+pokemon = "haunter"
 atributos = formativa1(pokemon)
 
 if atributos:
     print(f"{atributos["name"]}")
     print(f"{atributos["id"]}")
+    print(f"{atributos["height"]} ft")
+    print(f"{atributos["weight"]} lb")
